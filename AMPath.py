029a15da8f084cf0be018d587b018c88500cdd26
fonstein <ingrfjon@online.no>
2018-03-07 15:31:04 +0100

    Add sampling of face

---
+++ AMPath.py
@@ -0,0 +1,44 @@
+import FreeCAD
+import FreeCADGui
+import Part
+import Draft
+
+class SubObject(object):
+    sampling = []
+    def __init__(self, subObject):
+        self.subObject = subObject
+
+    def sampleSubObject(self, ustep, vstep, tolerance):
+        pRange = self.subObject.ParameterRange
+        umin = int(pRange[0])
+        umax = int(pRange[1])
+        vmin = int(pRange[2])
+        vmax = int(pRange[3])
+
+        """sample in u-v direction along sub object with given step in u anv v direction"""
+        for su in range(umin, umax+ustep, ustep):
+            for sv in range(vmin, vmax+vstep, vstep):
+                vec = self.subObject.valueAt(su,sv)
+                if self.subObject.isInside(vec,tolerance,True):
+                    self.sampling.append(vec)
+                else:
+                    pass
+
+    def displaySampling(self):
+        if len(self.sampling) > 0:
+            for vec in self.sampling:
+                Draft.makePoint(vec.x, vec.y, vec.z)
+        else:
+            FreeCAD.Console.PrintError("There are no points to display")
+
+def getSubObject():
+    try:
+        selected = FreeCADGui.Selection.getSelectionEx()
+        subObject = selected[0].SubObjects[0]
+        return subObject
+    except Exception as e:
+        FreeCAD.Console.PrintError("No object selected")
+
+sub = SubObject(getSubObject())
+sub.sampleSubObject(10, 10, 0.0)
+#sub.displaySampling()
